654f934d65bde1c717ef867386555d4b5778edab
Raul C. Rivero <rahulrcr98@gmail.com>
2022-07-27 14:31:11 -0400

    improve seo

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,7 +12,28 @@ export default {
   components: { Hero, Projects },
   metaInfo: {
     title: 'Raúl C. Rivero',
-    description: '@iocodz'
+    meta: [
+      {
+        name: 'author',
+        content: 'Raúl C. Rivero | @iocodz'
+      },
+      {
+        key: 'og:description',
+        name: 'og:description',
+        content: 'Software Engineer with 5+ years background passionate about developing user-friendly software applications.'
+      },
+      {
+        key: 'twitter:description',
+        name: 'twitter:description',
+        content: 'Software Engineer with 5+ years background passionate about developing user-friendly software applications.'
+      },
+      {
+        key: 'og:url',
+        name: 'og:url',
+        content: 'https://iocodz.me/picture.jpg',
+      }
+    ],
+
   }
 }
 </script>
